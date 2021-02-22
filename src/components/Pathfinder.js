6380f58243675584f46275af42de6408858ae877
Bhanu Gupta <bhanu.mbvg@gmail.com>
2021-02-22 23:23:37 +0530

    feat: add neighbours

--- src/components/Pathfinder.js
+++ src/components/Pathfinder.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Node from './Node';
 import './styles/Pathfinder.css';
 
-const cols = 25;
+const cols = 35;
 const rows = 10;
 
 const NODE_START_COL = 0
@@ -25,6 +25,7 @@ const Pathfinder = () => {
     }
     createSpot(grid);
     setGrid(grid);
+    addNeighbours(grid);
   };
 
   const createSpot = (grid) => {
@@ -43,7 +44,26 @@ const Pathfinder = () => {
     this.g = 0;
     this.isStart = this.x === NODE_START_ROW && this.y === NODE_START_COL;
     this.isEnd = this.x === NODE_END_ROW && this.y === NODE_END_COL;
+    this.neighbours = [];
+    this.previous = 0;
+    this.addneighbours = function(grid){
+      let i = this.x;
+      let j = this.y;
+      if( i > 0 ) this.neighbours.push( grid[i-1][j] );
+      if( i < rows - 1 ) this.neighbours.push( grid[i+1][j] );
+      if( j > 0 ) this.neighbours.push( grid[i][j-1] );
+      if( j < cols -1 ) this.neighbours.push( grid[i][j+1] );
+    }
   }
+
+  const addNeighbours = (grid) => {
+    for(let i = 0; i < rows; i++){
+      for(let j = 0; j < cols; j++){
+        grid[i][j].addneighbours(grid);
+      }
+    }
+  }
+
   //console.log(Grid);
   const nodeGrid = (
     <div>
